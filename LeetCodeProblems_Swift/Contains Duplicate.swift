1824613d57a161694c5fec7b85848e929ff8128b
Nagaraju0323 <nagarajukios85@gmail.com>
2024-01-29 22:01:40 +0530

    20. Valid Parentheses

--- LeetCodeProblems_Swift/Contains Duplicate.swift
+++ LeetCodeProblems_Swift/Contains Duplicate.swift
@@ -263,6 +263,27 @@ class Solution {
 
         }
     
+    
+    //MARK: - 20. Valid Parentheses
+    
+    func isValid(_ s: String) -> Bool {
+     
+        guard s.count % 2 == 0 else { return false }
+        var stack : [Character]  = []
+        for ch in s {
+            switch ch {
+            case "(" : stack.append(")")
+            case "{" : stack.append("}")
+            case "[" : stack.append("]")
+            default:
+                if stack.isEmpty || stack.removeLast() != ch {
+                    return false
+                }
+            }
+        }
+        return stack.isEmpty
+    }
+    
 }
 
 
